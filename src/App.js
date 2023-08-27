d59283f2d4fa141c5b407e2ec3f65fbe684e72ce
adithya0804 <skumaradithya0804@gmail.com>
2023-08-28 00:05:47 +0530

    seperated logic from component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 import CommentWidget from "./components/CommentWidget";
 import Dropdown from "./components/Dropdown";
 import { initialComments, userInfo } from "./utils/sampleData";
+import { v4 as uuidv4 } from "uuid";
 import styles from "./app.module.css";
+
 const userOptions = [
   {
     value: "2f19a5d9-0d0d-4bf3-9f9c-c51eb439af6f",
@@ -19,16 +21,116 @@ const userOptions = [
 ];
 function App() {
   const [user, setUser] = useState(userInfo[0].id);
+  const [comments, setComments] = useState(
+    JSON.parse(localStorage.getItem("comments")) || initialComments
+  );
+  /**
+   * Handles the editing of the comments
+   *
+   * @param { objectId } commentId
+   *   id of the comment to be edited
+   *
+   * @param { String } editText
+   *    the new edited comment text
+   *
+   * @returns { null }
+   *   since no return value is required it returns null and sets the comments object to updated one
+   *
+   */
+
+  const handleEdit = (commentId, editText) => {
+    const index = comments.findIndex((item) => item.id === commentId);
+    const updatedComments = [...comments];
+    updatedComments[index].comment = editText;
+    localStorage.setItem("comments", JSON.stringify(updatedComments));
+    setComments(updatedComments);
+  };
+
+  /**
+   * Handles the deleting of the comments
+   *
+   * @param { objectId} commentId
+   *   id of the comment to be deleted
+   * *
+   * @returns { null }
+   *   since no return value is required it returns null and sets the comments object to updated one
+   *
+   */
+
+  const handleDelete = (commentId) => {
+    const updatedComments = comments.filter((item) => {
+      return item.id !== commentId && item.parentCommentId !== commentId;
+    });
+    localStorage.setItem("comments", JSON.stringify(updatedComments));
+    setComments(updatedComments);
+  };
+
+  /**
+   * Handles the replyin to the comments
+   *
+   * @param { objectId} commentId
+   *   id of the comment to which the comment is being replied to, null in the case of new comments
+   * *
+   * @param { String} replyText
+   *   text content of the new added comment
+   * *
+   * @returns { null }
+   *   since no return value is required it returns null and sets the comments object to updated one
+   *
+   */
+
+  const handleReply = (parentCommentId, replyText) => {
+    const newReply = {
+      id: uuidv4(), // Generate unique commentId
+      comment: replyText,
+      postedBy: user,
+      postedOn: new Date().toISOString(),
+      likes: 0,
+      parentCommentId: parentCommentId,
+    };
+    // Update state with new reply
+    const updatedComments = [...comments, newReply];
+    setComments(updatedComments);
+
+    // Save updated comments to localStorage
+    localStorage.setItem("comments", JSON.stringify(updatedComments));
+  };
+
+  /**
+   * Handles the liking of the comments
+   *
+   * @param {objectId} commentId
+   *   id of the comment of which the likes have to be updated
+   * *
+   * @returns { null }
+   *   since no return value is required it returns null and sets the comments object to updated one after increasing likes
+   *
+   */
+  const onLike = (commentId) => {
+    const index = comments.findIndex((item) => item.id === commentId);
+    const likes = comments[index].likes;
+    const updatedComments = [...comments];
+    updatedComments[index].likes = likes + 1;
+    localStorage.setItem("comments", JSON.stringify(updatedComments));
+    setComments(updatedComments);
+  };
   const handleSelect = (value) => {
     setUser(value);
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.dropdown}>
         <span>Select User </span>{" "}
         <Dropdown onSelect={handleSelect} options={userOptions} />
       </div>
-      <CommentWidget initiaComments={initialComments} userId={user} />
+      <CommentWidget
+        handleDelete={handleDelete}
+        handleEdit={handleEdit}
+        handleReply={handleReply}
+        onLike={onLike}
+        comments={comments}
+      />
     </div>
   );
 }
--- src/components/CommentWidget/index.jsx
+++ src/components/CommentWidget/index.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import { v4 as uuidv4 } from "uuid";
 import Comment from "../Comment";
 import CommentInput from "../CommentInput";
 import Dropdown from "../Dropdown";
@@ -16,110 +15,28 @@ const sortOptions=[{
 
 /**
  *
- * @param { Array<Comments>} initialComments 
- *   Dummy array of comments
+  * @param { Function} handleEdit
+ *   function to handle edit process
  * *
- *  * @param { ObjectId} userId
- *   user id of the user interacting with widget
+ *  * @param { Function} onLike
+ *   function to handle likeing process
+ * *
+ *  * @param { Function} handleReply
+ *   function to handle replying process
+ * *
+ *  *  * @param { Function} handleDelete
+ *   function to handle deleting process
+ * *
+ * * @param {Array<Comment>} comments
  * *
  * @returns { JSX.Element  }
  *  JSX for comments widget
  *
  */
-const CommentWidget = ({ initialComments, userId }) => {
-const [comments, setComments]= useState(JSON.parse(localStorage.getItem('comments'))||initialComments);
+const CommentWidget = ({ handleDelete, handleEdit, handleReply, onLike, comments}) => {
 const [sort, setSort]=useState("postedOn");
 
-/**
- * Handles the editing of the comments
- *
- * @param { objectId } commentId
- *   id of the comment to be edited
- * 
- * @param { String } editText
- *    the new edited comment text
- *
- * @returns { null }
- *   since no return value is required it returns null and sets the comments object to updated one
- *
- */
-
-const handleEdit=(commentId, editText)=>{
-  const index = comments.findIndex(item => item.id === commentId);
-const updatedComments=[...comments];
-updatedComments[index].comment=editText
-localStorage.setItem('comments', JSON.stringify(updatedComments));
-setComments(updatedComments);
-}
-
-/**
- * Handles the deleting of the comments
- *
- * @param { objectId} commentId
- *   id of the comment to be deleted
- * *
- * @returns { null }
- *   since no return value is required it returns null and sets the comments object to updated one
- *
- */
-
-const handleDelete=(commentId)=>{
-const updatedComments=comments.filter(item=>{
-  return item.id!==commentId && item.parentCommentId!==commentId
-});
-localStorage.setItem('comments', JSON.stringify(updatedComments));
-setComments(updatedComments);
-}
-
-/**
- * Handles the replyin to the comments
- *
- * @param { objectId} commentId
- *   id of the comment to which the comment is being replied to, null in the case of new comments
- * *
- * @param { String} replyText
- *   text content of the new added comment
- * *
- * @returns { null }
- *   since no return value is required it returns null and sets the comments object to updated one
- *
- */
-
-const handleReply = (parentCommentId, replyText) => {
-  const newReply = {
-    id: uuidv4(), // Generate unique commentId
-    comment: replyText,
-    postedBy: userId,
-    postedOn: new Date().toISOString(),
-    likes: 0,
-    parentCommentId: parentCommentId,
-  };
-  // Update state with new reply
-  const updatedComments = [...comments, newReply];
-  setComments(updatedComments);
-
-  // Save updated comments to localStorage
-  localStorage.setItem('comments', JSON.stringify(updatedComments));
-};
-
-/**
- * Handles the liking of the comments
- *
- * @param {objectId} commentId
- *   id of the comment of which the likes have to be updated
- * *
- * @returns { null }
- *   since no return value is required it returns null and sets the comments object to updated one after increasing likes
- *
- */
-const onLike=(commentId)=>{  
-  const index = comments.findIndex(item => item.id === commentId);
-const likes=comments[index].likes;
-const updatedComments=[...comments];
-updatedComments[index].likes=likes+1;
-localStorage.setItem('comments', JSON.stringify(updatedComments));
-setComments(updatedComments);
-}
+//to handle the sorting value by default it is date
 const handleSelect=(value)=>{
 setSort(value)
 }
